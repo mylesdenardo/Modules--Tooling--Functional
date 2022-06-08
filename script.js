bd0ef36b40b99036dc8554d13e7dd053d4e68550
mylesdenardo <themylesd@gmail.com>
2022-06-08 17:18:08 -0400

    the module pattern

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 // Importing module
+
+/*
 // import { addToCart, totalPrice as price, tq } from './shoppingCart.js';
 
 // addToCart('bread', 5);
@@ -39,3 +41,33 @@ console.log(lastPost);
 
 const lastPost2 = await getLastPost();
 console.log(lastPost2);
+*/
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 237;
+  const totalQuantity = 23;
+
+  const addToCart = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} added to cart`);
+  };
+
+  const orderStock = function (product, quantity) {
+    cart.push({ product, quantity });
+    console.log(`${quantity} ${product} ordered from supplier`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apples', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
+console.log(ShoppingCart2.shippingCost); // undefined
