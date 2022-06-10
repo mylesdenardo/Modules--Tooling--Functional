eceda4b2c724970a664cc1beed1fdf00f1ca5767
mylesdenardo <themylesd@gmail.com>
2022-06-10 13:19:15 -0400

    NPM
    parcel and npm scripts

--- script.js
+++ script.js
@@ -43,6 +43,10 @@ const lastPost2 = await getLastPost();
 console.log(lastPost2);
 */
 
+/*
+//////////////////////////////////////////////////////////////
+// The Module Pattern
+
 const ShoppingCart2 = (function () {
   const cart = [];
   const shippingCost = 10;
@@ -71,3 +75,26 @@ ShoppingCart2.addToCart('apples', 4);
 ShoppingCart2.addToCart('pizza', 2);
 console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost); // undefined
+*/
+
+import cloneDeep from './node_modules/lodash-es/cloneDeep.js';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+if (module.hot) {
+  module.hot.accept();
+}
